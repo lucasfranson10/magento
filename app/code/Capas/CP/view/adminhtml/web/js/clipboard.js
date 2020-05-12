4ad9890d335667cb1c521a12a005f497dddc4aee
Lucas Franson <lucasfranson27@gmail.com>
2020-05-12 18:12:40 +0100

    funcionality works, miss fixs the layout of the text

--- app/code/Capas/CP/view/adminhtml/web/js/clipboard.js
+++ app/code/Capas/CP/view/adminhtml/web/js/clipboard.js
@@ -3,8 +3,12 @@ require([
 ],function($){
    // $('#order_myaction').on('click', clipboard());
     function clipboard(message){
-        message.select();
+        var dummy = document.createElement("textarea");
+        document.body.appendChild(dummy);
+        dummy.value = message;
+        dummy.select();
         document.execCommand("copy");
+        document.body.removeChild(dummy);
         alert("Copied the text: " + message);
     }
     window.clipboard = clipboard;
